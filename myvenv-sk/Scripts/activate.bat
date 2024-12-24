b1de688ae6e5df18f2c7363de5a0e598f701f04f
muted-collective <dkdrabile@gmail.com>
2024-12-24 12:26:33 +0200

    Final commit 6

--- myvenv-sk/Scripts/activate.bat
+++ myvenv-sk/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\legal_api - SK\myvenv-sk
+set VIRTUAL_ENV=C:\Users\dkdra\Desktop\Nappy Labs\Projects\legal_api - SK\myvenv-sk
 
 if not defined PROMPT set PROMPT=$P$G
 
